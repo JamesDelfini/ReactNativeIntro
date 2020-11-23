c2259d8ecacb574cf72ed73a4c58c95a3a9087b7
Dennis James Matildo <dj.matildo@gmail.com>
2020-11-23 13:07:49 +0800

    Setup the Shopping List Header

--- components/Header.js
+++ components/Header.js
@@ -1,37 +1,29 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Header = ({ title }) => {
   return (
-    <View style={styles.container}>
+    <View style={styles.header}>
       <Text style={styles.text}>{title}</Text>
-      <Image
-        source={{ uri: 'https://randomuser.me/api/portraits/men/1.jpg' }}
-        style={styles.img}
-      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkslateblue',
   },
   text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    color: '#FFF',
+    fontSize: 23,
+    textAlign: 'center',
   },
 })
 
 Header.defaultProps = {
-  title: 'Default props is, Hello!',
+  title: 'Shopping List',
 }
 
 export default Header
