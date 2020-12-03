00302c14399b1b9299e4e9b648be66a289728fec
Dennis James Matildo <dj.matildo@gmail.com>
2020-12-04 04:47:39 +0800

    Sample Rendering of a Data of List

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, FlatList } from 'react-native'
+import 'react-native-get-random-values'
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuidv4(),
+      text: 'Milk',
+    },
+    {
+      id: uuidv4(),
+      text: 'Eggs',
+    },
+    {
+      id: uuidv4(),
+      text: 'Bread',
+    },
+    {
+      id: uuidv4(),
+      text: 'Juice',
+    },
+  ])
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <Text>{item.text}</Text>}
+      />
     </View>
   )
 }
